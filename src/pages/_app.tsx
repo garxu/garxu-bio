fcbb5e570045bdf08db42bb3123425465b0d2125
trezzuun <icherryvan19@gmail.com>
2025-03-23 15:31:48 -0400

    testting 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/globals.scss';
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
